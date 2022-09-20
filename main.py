0dcc77957302d61078dc117e7a1a6d758ef8a012
gcodd <112970143+gcodd@users.noreply.github.com>
2022-09-20 15:32:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,78 +2,66 @@
 from calendar import c
 from typing import Dict, Union
 
-# Import the functions we wrote in procedural_resale_shop.py
-from procedural_resale_shop import buy, update_price, sell, print_inventory, refurbish
-
-""" This helper function takes in a bunch of information about a computer,
-    and packages it up into a python dictionary to make it easier to store
-
-    Note: because python is dynamically typed, you may not be used to seeing 
-    explicit data types (str, int, etc.) listed in a python function. We're 
-    going to go the extra step, because when we get to Java it'll be required!
-"""
-def create_computer(description: str,
-                    processor_type: str,
-                    hard_drive_capacity: int,
-                    memory: int,
-                    operating_system: str,
-                    year_made: int,
-                    price: int):
-    return {'description': description,
-            'processor_type': processor_type,
-            'hard_drive_capacity': hard_drive_capacity,
-            'memory': memory,
-            'operating_system': operating_system,
-            'year_made': year_made,
-            'price': price
-    }
+# Import classes Computer and ResaleShop so their instance methods can be used
+from computer import Computer
+from oo_resale_shop import ResaleShop
 
 def main():
     
-    # First, let's make a computer
-    computer = create_computer(
-        "Mac Pro (Late 2013)",
-        "3.5 GHc 6-Core Intel Xeon E5",
-        1024, 64,
-        "macOS Big Sur", 2013, 1500
-    )
-
     # Print a little banner
     print("-" * 21)
     print("COMPUTER RESALE STORE")
     print("-" * 21)
 
-    # Add it to the resale store's inventory
-    print("Buying", computer["description"])
+    # Instantiate resale shop object, assign to variable "shop"
+    shop = ResaleShop()
+
+    # Instantiate computer object, assign to variable "computer_obj"
+    # Pass arguments for all attributes
+    computer = Computer("Mac Pro (Late 2013)",
+                        "3.5 GHc 6-Core Intel Xeon E5",
+                        1024, 64,
+                        "macOS Big Sur", 2013, 1500)
+
+    # Add computer to store's inventory                    
+    print("Buying", computer.description)
     print("Adding to inventory...")
-    computer_id = buy(computer)
+    # Call buy instance method for shop object, pass computer object as argument
+    # Assign return value to computer_id
+    computer_id = shop.buy(computer)
     print("Done.\n")
 
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
-    print("Done.\n")
+    #Call print_inventory instance method 
+    shop.print_inventory()
+    print("Done.\n")    
 
     # Now, let's refurbish it
+    #Store name of new OS in new_OS variable
     new_OS = "MacOS Monterey"
     print("Refurbishing Item ID:", computer_id, ", updating OS to", new_OS)
     print("Updating inventory...")
-    refurbish(computer_id, new_OS)
+    # Call refurbish instance method from shop object
+    # Pass computer_id and new_OS as args
+    shop.refurbish(computer_id, new_OS)
     print("Done.\n")
 
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    # Call print_inventory instance method from shop object
+    shop.print_inventory()
     print("Done.\n")
-    
+
     # Now, let's sell it!
     print("Selling Item ID:", computer_id)
-    sell(computer_id)
-    
+    #Call sell instance method from shop object, pass computer_id as arg
+    shop.sell(computer_id)
+
     # Make sure it worked by checking inventory
     print("Checking inventory...")
-    print_inventory()
+    #Call print_inventory instance method from shop object
+    shop.print_inventory()
     print("Done.\n")
-
-# Calls the main() function when this file is run
+    
 if __name__ == "__main__": main()
